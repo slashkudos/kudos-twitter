3739ffc1751ce0c3d11c90c2beb9fcf9468248f7
Philip Gai <PhilipMGai@gmail.com>
2021-12-28 20:36:56 +0000

    Add CmdletBinding to Setup-Codespaces.ps1

--- scripts/Setup-Codespaces.ps1
+++ scripts/Setup-Codespaces.ps1
@@ -8,5 +8,8 @@
     Installs required dev tools and configurations.
 #>
 
+[CmdletBinding()]
+Param()
+
 # Clone helper twitter account-activity-dashboard tool
 gh repo clone twitterdev/account-activity-dashboard /workspaces/twitterdev-account-activity-dashboard
