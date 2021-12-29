60634027b2b09a62a6b557b8fe37a372bfa72ccc
Philip Gai <PhilipMGai@gmail.com>
2021-12-29 06:43:54 +0000

    Implement registration e2e using twurl

--- scripts/Register-AccountActivityApi.ps1
+++ scripts/Register-AccountActivityApi.ps1
@@ -2,25 +2,56 @@
 
 <#
     .SYNOPSIS
-    Registers a webhook URL for the Account Activity API.
+    Registers a webhook URL and subscribes to all events for the Account Activity API.
 
     .DESCRIPTION
     Registers a webhook URL for the Account Activity API.
     See https://developer.twitter.com/en/docs/tutorials/getting-started-with-the-account-activity-api for docs on how to setup the endpoint.
-    Requires elevated access.
 #>
 
 [CmdletBinding()]
 Param(
+    [Parameter(Mandatory=$true)]
+    [string]$ApiKey,
+
+    [Parameter(Mandatory=$true)]
+    [string]$ApiSecret,
+
+    [Parameter(Mandatory=$true)]
+    [string]$AccessToken,
+
     [string]$Environment = "production",
-    [string]$WebhookUrl = "https://restapi.slashkudos.com/twitter/webhooks",
-    [string]$ApiKey = $env:twitter_api_key,
-    [string]$AccessToken = $env:twitter_access_token
+    [string]$WebhookUrl = "https://restapi.slashkudos.com/twitter/webhooks"
 )
 
-Write-Debug "API Key: $ApiKey"
-Write-Debug "Access Token: $AccessToken"
+if (-not $ApiKey) {
+    throw "API Key is required"
+}
+if (-not $ApiSecret) {
+    throw "API Secret is required"
+}
+if (-not $AccessToken) {
+    throw "Access Token is required"
+}
 
-curl --request POST `
- --url "https://api.twitter.com/1.1/account_activity/all/:$Environment/webhooks.json?url=$WebhookUrl" `
- --header "authorization: OAuth oauth_consumer_key=`"$ApiKey`", oauth_nonce=`"GENERATED`", oauth_signature=`"GENERATED`", oauth_signature_method=`"HMAC-SHA1`", oauth_timestamp=`"GENERATED`", oauth_token=`"$AccessToken`", oauth_version=`"1.0`""
+twurl authorize --bearer --consumer-key $ApiKey --consumer-secret $ApiSecret
+
+Write-Host "Webhooks before..."
+twurl /1.1/account_activity/all/webhooks.json
+
+Write-Host "`nRegistering the webhook (/1.1/account_activity/all/$Environment/webhooks.json?url=$WebhookUrl)..."
+twurl -X POST "/1.1/account_activity/all/$Environment/webhooks.json?url=$WebhookUrl"
+
+Write-Host "`nWebhooks after..."
+twurl /1.1/account_activity/all/webhooks.json
+
+$webhookId = Read-Host "`nPlease enter the webhook id to validate"
+
+Write-Host "Validating webhook.."
+twurl -X PUT "/1.1/account_activity/all/$Environment/webhooks/$webhookId.json"
+
+Write-Host "Creating a subscription for the webhook (/1.1/account_activity/all/$Environment/subscriptions.json)..."
+twurl -X POST "/1.1/account_activity/all/$Environment/subscriptions.json"
+
+Write-Host "`nListing the current All Activity type subscriptions..."
+twurl --bearer "/1.1/account_activity/all/$Environment/subscriptions/list.json"
