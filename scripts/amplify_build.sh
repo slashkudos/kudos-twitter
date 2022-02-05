ea6e02fb5e2366d1f535c969af75c855f592fee5
Philip Gai <PhilipMGai@gmail.com>
2022-02-05 21:05:43 +0000

    Update amplify_build.sh Simplify Actions workflow

--- scripts/amplify_build.sh
+++ scripts/amplify_build.sh
@@ -1,11 +1,19 @@
+npm ci
+echo "Executing npm ci in $(pwd)"
+
 cd amplify/backend/function/twitterwebhookshandler/lib
+echo "Executing npm ci, lint and build in $(pwd)"
 npm ci
 npm run lint
 npm run build
 cd -
 
+echo "Copying lib/.npmrc into src folder"
 cp amplify/backend/function/twitterwebhookshandler/lib/.npmrc amplify/backend/function/twitterwebhookshandler/src
 
 cd amplify/backend/function/twitterwebhookshandler/src
+echo "Executing npm ci --production in $(pwd)"
 npm ci --production
 cd -
+
+echo "Done"
