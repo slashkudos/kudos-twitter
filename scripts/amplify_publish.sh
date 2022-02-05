4206c07105179714fcfd20b68d867b18ee91f0b8
Philip Gai <PhilipMGai@gmail.com>
2022-02-05 09:27:47 +0000

    Fix npm ci with github packages

--- scripts/amplify_publish.sh
+++ scripts/amplify_publish.sh
@@ -4,6 +4,8 @@ npm run lint
 npm run build
 cd -
 
+cp amplify/backend/function/twitterwebhookshandler/lib/.npmrc amplify/backend/function/twitterwebhookshandler/src
+
 cd amplify/backend/function/twitterwebhookshandler/src
 npm ci --production
 cd -
