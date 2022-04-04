12fb01865177807d7eaa8a7cdb10f10f0094dd2b
Philip Gai <PhilipMGai@gmail.com>
2022-04-04 08:19:07 +0000

    Update kudos count message

--- amplify/backend/function/twitterwebhookshandler/lib/index.ts
+++ amplify/backend/function/twitterwebhookshandler/lib/index.ts
@@ -97,8 +97,15 @@ export async function handler(event: APIGatewayEvent): Promise<APIGatewayProxyRe
           });
 
           const kudosCount = await kudosApiClient.getTotalKudosForReceiver(receiverUsername, DataSourceApp.twitter);
-          let tweetResponse = `Congrats @${receiverUsername}, you received Kudos from @${giverUsername}! You now have ${kudosCount} Kudos! 🎉 💖`;
-          tweetResponse += `\nView kudos: https://app.slashkudos.com/?search=${receiverUsername}`;
+
+          let kudosCountMessage = `You now have ${kudosCount} Kudos!`;
+          if (kudosCount === 1) {
+            kudosCountMessage = `This is your first Kudo!`;
+          }
+
+          let tweetResponse = `Congrats @${receiverUsername}, you received Kudos from @${giverUsername}! ${kudosCountMessage} 🎉 💖`;
+          tweetResponse += `\nView your kudos: https://app.slashkudos.com/?search=${receiverUsername}`;
+
           logger.info(`Replying to tweet (${tweet.id_str}) with "${tweetResponse}"`);
 
           await client.v1.reply(tweetResponse, tweet.id_str, { auto_populate_reply_metadata: true });
