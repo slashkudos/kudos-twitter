3b8dccb15f9dc9cae3487a456334167e5f7886a8
Philip Gai <philip@slashkudos.com>
2022-05-25 23:18:36 -0500

    Like the tweet before responding (#21)
    Also:
    - breakout methods likeTweet and respondToTweet
    - extract and rename some variables

--- amplify/backend/function/twitterwebhookshandler/lib/TweetCreateEventsHandler.ts
+++ amplify/backend/function/twitterwebhookshandler/lib/TweetCreateEventsHandler.ts
@@ -1,7 +1,7 @@
 import { APIGatewayProxyResultV2 } from "aws-lambda";
 import { LoggerService } from "./LoggerService";
-import TwitterApi from "twitter-api-v2";
-import { TweetCreateEventActivity } from "./types/twitter-types";
+import TwitterApi, { UserV1 } from "twitter-api-v2";
+import { Tweet, TweetCreateEventActivity } from "./types/twitter-types";
 import { DataSourceApp, KudosApiClient } from "@slashkudos/kudos-api";
 import Utilities from "./Utilities";
 
@@ -15,15 +15,19 @@ export default class TweetCreateEventsActivityHandler {
   ): Promise<APIGatewayProxyResultV2<never>> {
     const appUser = await twitterClient.currentUser();
     const tweet = tweetCreateEventActivity.tweet_create_events[0];
+    const giverScreenName = tweet.user.screen_name;
+    const appScreenName = appUser.screen_name;
+
+    await TweetCreateEventsActivityHandler.likeTweet(tweet, appUser, twitterClient);
 
     // Remove beginning mentions if tweet is a reply (these mentions are added automatically)
-    const lastAppMentionIndex = tweet.text.lastIndexOf(`@${appUser.screen_name}`);
+    const lastAppMentionIndex = tweet.text.lastIndexOf(`@${appScreenName}`);
     const mentions = tweet.entities.user_mentions.filter((mention) => mention.id !== appUser.id && mention.indices[0] > lastAppMentionIndex);
     const tweetText = tweet.text.substring(lastAppMentionIndex);
 
-    logger.info(`Checking if the tweet "${tweetText}" starts with "@${appUser.screen_name}" and contains mentions.`);
+    logger.info(`Checking if the tweet "${tweetText}" starts with "@${appScreenName}" and contains mentions.`);
     const isUserGivingKudos =
-      tweetText.startsWith(`@${appUser.screen_name}`) && mentions.length > 0 && (!tweet.in_reply_to_status_id || tweet.in_reply_to_user_id !== appUser.id);
+      tweetText.startsWith(`@${appScreenName}`) && mentions.length > 0 && (!tweet.in_reply_to_status_id || tweet.in_reply_to_user_id !== appUser.id);
     if (!isUserGivingKudos) {
       return Utilities.createApiResult("Tweet is not someone giving someone kudos.", 200);
     }
@@ -32,17 +36,17 @@ export default class TweetCreateEventsActivityHandler {
 
     for (const mention of mentions) {
       try {
-        const { giverUsername, receiverUsername } = { giverUsername: tweet.user.screen_name, receiverUsername: mention.screen_name };
-        if (kudosCache[receiverUsername]) continue;
-        kudosCache[receiverUsername] = true;
+        const receiverScreenName = mention.screen_name;
+        if (kudosCache[receiverScreenName]) continue;
+        kudosCache[receiverScreenName] = true;
 
         logger.info("Getting receiver user profile.");
         const receiverProfile = await twitterClient.v2.user(mention.id_str, { "user.fields": ["profile_image_url"] });
         logger.http(JSON.stringify(receiverProfile));
 
         await kudosApiClient.createKudo({
-          giverUsername,
-          receiverUsername,
+          giverUsername: giverScreenName,
+          receiverUsername: receiverScreenName,
           message: tweetText,
           tweetId: tweet.id_str,
           giverProfileImageUrl: tweet.user.profile_image_url_https,
@@ -50,25 +54,37 @@ export default class TweetCreateEventsActivityHandler {
           dataSource: DataSourceApp.twitter,
         });
 
-        const kudosCount = await kudosApiClient.getTotalKudosForReceiver(receiverUsername, DataSourceApp.twitter);
-
-        let kudosCountMessage = `You now have ${kudosCount} kudos!`;
-        if (kudosCount === 1) {
-          kudosCountMessage = `This is your first kudo!`;
-        }
-
-        let tweetResponse = `Congrats @${receiverUsername}, you received kudos from @${giverUsername}! ${kudosCountMessage} 🎉 💖`;
-        tweetResponse += `\nView your kudos: https://app.slashkudos.com/?search=${receiverUsername}`;
-
-        logger.info(`Replying to tweet (${tweet.id_str}) with "${tweetResponse}"`);
-
-        await twitterClient.v1.reply(tweetResponse, tweet.id_str, { auto_populate_reply_metadata: true });
+        await TweetCreateEventsActivityHandler.respondToTweet(kudosApiClient, receiverScreenName, giverScreenName, tweet, twitterClient);
       } catch (error) {
         Utilities.logError(error, `Failed giving kudos to ${mention.screen_name}`);
       }
     }
+    return Utilities.createApiResult("Succesfully gave kudos", 200);
+  }
+
+  private static async respondToTweet(
+    kudosApiClient: KudosApiClient,
+    receiverScreenName: string,
+    giverScreenName: string,
+    tweet: Omit<Tweet, "author_id">,
+    twitterClient: TwitterApi
+  ): Promise<void> {
+    const kudosCount = await kudosApiClient.getTotalKudosForReceiver(receiverScreenName, DataSourceApp.twitter);
+
+    let kudosCountMessage = `You now have ${kudosCount} kudos!`;
+    if (kudosCount === 1) {
+      kudosCountMessage = `This is your first kudo!`;
+    }
+
+    let tweetResponse = `Congrats @${receiverScreenName}, you received kudos from @${giverScreenName}! ${kudosCountMessage} 🎉 💖`;
+    tweetResponse += `\nView your kudos: https://app.slashkudos.com/?search=${receiverScreenName}`;
+
+    logger.info(`Replying to tweet (${tweet.id_str}) with "${tweetResponse}"`);
+    await twitterClient.v1.reply(tweetResponse, tweet.id_str, { auto_populate_reply_metadata: true });
+  }
+
+  private static async likeTweet(tweet: Omit<Tweet, "author_id">, appUser: UserV1, twitterClient: TwitterApi): Promise<void> {
     try {
-      // Don't be a weirdo and like your own tweet
       if (tweet.user.id !== appUser.id) {
         logger.info("Liking the tweet.");
         await twitterClient.v2.like(appUser.id_str, tweet.id_str);
@@ -76,6 +92,5 @@ export default class TweetCreateEventsActivityHandler {
     } catch (error) {
       Utilities.logError(error, "Error liking the tweet.");
     }
-    return Utilities.createApiResult("Succesfully gave kudos", 200);
   }
 }
