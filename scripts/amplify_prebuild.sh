2a5c1fe34cec2e2f9fd982f63cf2d42fc0b77bc9
Philip Gai <philip@slashkudos.com>
2022-05-27 19:49:41 +0000

    Update amplify_prebuild.sh

--- scripts/amplify_prebuild.sh
+++ scripts/amplify_prebuild.sh
@@ -1,13 +1,14 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-npm ci
 echo "Executing npm ci in $(pwd)"
+npm ci
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 ROOT_DIR=$SCRIPT_DIR/..
+FUNCTION_DIR=amplify/backend/function/twitterwebhookshandler
 
-cd $ROOT_DIR/amplify/backend/function/twitterwebhookshandler/lib
+cd $ROOT_DIR/$FUNCTION_DIR/lib
 echo "Executing npm ci, lint and build in $(pwd)"
 npm ci
 npm run lint
@@ -15,9 +16,9 @@ npm run build
 cd -
 
 echo "Copying lib/.npmrc into src folder"
-cp $ROOT_DIR/amplify/backend/function/twitterwebhookshandler/lib/.npmrc $ROOT_DIR/amplify/backend/function/twitterwebhookshandler/src
+cp $ROOT_DIR/$FUNCTION_DIR/lib/.npmrc $ROOT_DIR/$FUNCTION_DIR/src
 
-cd $ROOT_DIR/amplify/backend/function/twitterwebhookshandler/src
+cd $ROOT_DIR/$FUNCTION_DIR/src
 echo "Executing npm ci --production in $(pwd)"
 npm ci --production
 cd -
